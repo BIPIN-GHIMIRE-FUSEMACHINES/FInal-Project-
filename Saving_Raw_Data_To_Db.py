ee2f42b44ac086309c27959c95ef791f917f2f1f
BIPIN-GHIMIRE-FUSEMACHINES <bipin.ghimire@fusemachines.com>
2023-11-02 23:33:49 +0545

    Python Script for saving the raw data to database added.

--- Saving_Raw_Data_To_Db.py
+++ Saving_Raw_Data_To_Db.py
@@ -1,12 +1,25 @@
-
 from pyspark.sql import SparkSession
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 spark = SparkSession.builder.appName("CSVtoPostgres")\
                             .config('spark.driver.extraClassPath','/usr/lib/jvm/java-17-openjdk-amd64/lib/postgresql-42.6.0.jar')\
                             .getOrCreate()
 
-uncleaned_data = spark.read.csv("./Data/Raw_Data/googleplaystore.csv", header=True, inferSchema=True)
+try:
+    uncleaned_data = spark.read.csv("/home/bipin/FInal-Project-/Data/Raw_Data/googleplaystoree.csv", header=True, inferSchema=True)
+    logger.info("************************")
+    logger.info("CSV File successfully read.")
+    logger.info("*************************")
 
+except Exception as e:
+    logger.error("Coundnot Find the CSV File: %s", str(e))
 # uncleaned_data.show()
 
 from dotenv import load_dotenv
@@ -19,8 +32,11 @@ password = os.getenv("password")
 
 jdbc_url = "jdbc:postgresql://localhost:5432/Apps_Database"
 
-uncleaned_data.write.format('jdbc').options(url=jdbc_url,driver = 'org.postgresql.Driver', dbtable = 'apps_raw_data_table', user=user,password=password).mode('overwrite').save()
-
+try:
+    uncleaned_data.write.format('jdbc').options(url=jdbc_url,driver = 'org.postgresql.Driver', dbtable = 'apps_raw_data_table', user=user,password=password).mode('overwrite').save()
+    logger.info("Raw Data successfully loaded to DB")
+except Exception as e:
+    logger.error("Couldnot load data to postgres: %s",str(e))
 
 
 
