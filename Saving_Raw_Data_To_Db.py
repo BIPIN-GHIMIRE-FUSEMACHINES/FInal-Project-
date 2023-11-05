533e6d2a05a792037e9a1618eaa2620903369e53
BIPIN-GHIMIRE-FUSEMACHINES <bipin.ghimire@fusemachines.com>
2023-11-05 11:00:12 +0545

    Data Loading to Postgres Changes made.

--- Saving_Raw_Data_To_Db.py
+++ Saving_Raw_Data_To_Db.py
@@ -1,10 +1,21 @@
 from pyspark.sql import SparkSession
 import logging
+import pyspark.sql.functions as f
+from pyspark.sql.types import StructField, StringType, StructType, BooleanType, FloatType, IntegerType, DateType
+from dotenv import load_dotenv
+import os
+import yaml
+
+yaml_file_path = '/home/bipin/FInal-Project-/config.yaml'
+
+# Read the YAML file and parse it into a Python dictionary
+with open(yaml_file_path, 'r') as yaml_file:
+    config = yaml.safe_load(yaml_file)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch = logging.StreamHandler()
+ch = logging.FileHandler("/home/bipin/FInal-Project-/test.log")
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
@@ -12,8 +23,24 @@ spark = SparkSession.builder.appName("CSVtoPostgres")\
                             .config('spark.driver.extraClassPath','/usr/lib/jvm/java-17-openjdk-amd64/lib/postgresql-42.6.0.jar')\
                             .getOrCreate()
 
+custom_schema = StructType([
+    StructField("App", StringType(), True),
+    StructField("Category", StringType(), True),
+    StructField("Rating", FloatType(), True),
+    StructField("Reviews", IntegerType(), True),
+    StructField("Size", StringType(), True),
+    StructField("Installs", StringType(), True),
+    StructField("Type", StringType(), True),
+    StructField("Price", StringType(), True),
+    StructField("Content Rating", StringType(), True),
+    StructField("Genres", StringType(), True),
+    StructField("Last Updated", StringType(), True),
+    StructField("Current Ver", StringType(), True),
+    StructField("Android Ver", StringType(), True),
+])
+
 try:
-    uncleaned_data = spark.read.csv("/home/bipin/FInal-Project-/Data/Raw_Data/googleplaystoree.csv", header=True, inferSchema=True)
+    uncleaned_data = spark.read.csv(config["Raw_data_path"], header=True, inferSchema=True)
     logger.info("************************")
     logger.info("CSV File successfully read.")
     logger.info("*************************")
@@ -22,8 +49,11 @@ except Exception as e:
     logger.error("Coundnot Find the CSV File: %s", str(e))
 # uncleaned_data.show()
 
-from dotenv import load_dotenv
-import os
+spark.conf.set("spark.sql.legacy.timeParserPolicy","LEGACY")
+
+# data = uncleaned_data.withColumn("Last Updated", regexp_replace(col("Last Updated"), "Jan", "January"))  # Replace abbreviated month names
+data = uncleaned_data.withColumn("Last Updated", f.regexp_replace(f.col("Last Updated"), ",", ""))  # Remove commas
+data = data.withColumn("Last Updated", f.to_date(f.col("Last Updated"), "MMMM d yyyy"))
 
 load_dotenv()
 
@@ -33,7 +63,7 @@ password = os.getenv("password")
 jdbc_url = "jdbc:postgresql://localhost:5432/Apps_Database"
 
 try:
-    uncleaned_data.write.format('jdbc').options(url=jdbc_url,driver = 'org.postgresql.Driver', dbtable = 'apps_raw_data_table', user=user,password=password).mode('overwrite').save()
+    data.write.format('jdbc').options(url=jdbc_url,driver = 'org.postgresql.Driver', dbtable = 'apps_raw_data_table', user=user,password=password).mode('overwrite').save()
     logger.info("Raw Data successfully loaded to DB")
 except Exception as e:
     logger.error("Couldnot load data to postgres: %s",str(e))
