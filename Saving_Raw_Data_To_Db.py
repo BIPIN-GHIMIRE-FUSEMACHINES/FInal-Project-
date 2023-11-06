6fb80f8fdd87865433fe4fae368533531f235181
BIPIN-GHIMIRE-FUSEMACHINES <bipin.ghimire@fusemachines.com>
2023-11-06 09:10:47 +0545

    Update in saving script.

--- Saving_Raw_Data_To_Db.py
+++ Saving_Raw_Data_To_Db.py
@@ -5,22 +5,31 @@ from pyspark.sql.types import StructField, StringType, StructType, BooleanType, 
 from dotenv import load_dotenv
 import os
 import yaml
+import datetime
 
-yaml_file_path = '/home/bipin/FInal-Project-/config.yaml'
+load_dotenv()
+raw_data_path = os.getenv("Raw_data_path")
 
-# Read the YAML file and parse it into a Python dictionary
-with open(yaml_file_path, 'r') as yaml_file:
-    config = yaml.safe_load(yaml_file)
+log_folder = os.getenv("log_folder_path")
+
+if not os.path.exists(log_folder):
+    os.mkdir(log_folder)
+
+current_datetime = datetime.datetime.now()
+log_filename = current_datetime.strftime("%Y-%m-%d.log")
+
+log_file_path = os.path.join(log_folder,log_filename)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch = logging.FileHandler("/home/bipin/FInal-Project-/test.log")
+ch = logging.FileHandler(log_file_path, mode = 'w')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+jar_file_path = os.getenv("postgres_jar_file_path")
 spark = SparkSession.builder.appName("CSVtoPostgres")\
-                            .config('spark.driver.extraClassPath','/usr/lib/jvm/java-17-openjdk-amd64/lib/postgresql-42.6.0.jar')\
+                            .config('spark.driver.extraClassPath',jar_file_path)\
                             .getOrCreate()
 
 custom_schema = StructType([
@@ -39,11 +48,10 @@ custom_schema = StructType([
     StructField("Android Ver", StringType(), True),
 ])
 
+logger.info("Reading Raw Data")
 try:
-    uncleaned_data = spark.read.csv(config["Raw_data_path"], header=True, inferSchema=True)
-    logger.info("************************")
+    uncleaned_data = spark.read.csv(raw_data_path, header=True, inferSchema=True)
     logger.info("CSV File successfully read.")
-    logger.info("*************************")
 
 except Exception as e:
     logger.error("Coundnot Find the CSV File: %s", str(e))
@@ -55,13 +63,14 @@ spark.conf.set("spark.sql.legacy.timeParserPolicy","LEGACY")
 data = uncleaned_data.withColumn("Last Updated", f.regexp_replace(f.col("Last Updated"), ",", ""))  # Remove commas
 data = data.withColumn("Last Updated", f.to_date(f.col("Last Updated"), "MMMM d yyyy"))
 
-load_dotenv()
 
-user = os.getenv("user")
-password = os.getenv("password")
+
+user = os.getenv("postgres_username")
+password = os.getenv("postgres_password")
 
 jdbc_url = "jdbc:postgresql://localhost:5432/Apps_Database"
 
+logger.info("Dumping the raw data to postgres database using custome schema.")
 try:
     data.write.format('jdbc').options(url=jdbc_url,driver = 'org.postgresql.Driver', dbtable = 'apps_raw_data_table', user=user,password=password).mode('overwrite').save()
     logger.info("Raw Data successfully loaded to DB")
