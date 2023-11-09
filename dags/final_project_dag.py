9cfbe8bc479427a0342b5755f102d72a385bbec2
BIPIN-GHIMIRE-FUSEMACHINES <bipin.ghimire@fusemachines.com>
2023-11-09 09:43:02 +0545

    Final airflow dag uploaded.

--- dags/final_project_dag.py
+++ dags/final_project_dag.py
@@ -2,14 +2,13 @@ from datetime import datetime
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash_operator import BashOperator
-from airflow.contrib.sensors.file_sensor import FileSensor
 from airflow.models import Variable
 import pandas as pd
 import os
 import logging
 import pandas as pd
 
-logger = logging.getLogger(_name_)
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch = logging.StreamHandler()
@@ -21,82 +20,67 @@ extraction_script = Variable.get("extraction_script")
 transformation_script = Variable.get("Transformation_Script")
 Business_metrics = Variable.get("Business_metrics_script")
 
+def process_csv_files():
 
-def file_type_validation(**kwargs):
+    input_folder = Variable.get("input_folder")
+    output_folder = Variable.get("output_folder")
+
+    columns_to_check = ["App", "Category", "Rating", "Reviews", "Size", "Installs",
+                    "Type", "Price", "Content Rating", "Genres", "Last Updated",
+                    "Current Ver", "Android Ver"]
+    
+    csv_files = [filename for filename in os.listdir(input_folder) if filename.endswith('.csv')]
+
+    for filename in csv_files:
+        file_path = os.path.join(input_folder, filename)
+        try:
+            df = pd.read_csv(file_path)
+            
+            if all(column in df.columns for column in columns_to_check) and len(df.columns) == len(columns_to_check):
+                output_file_path = os.path.join(output_folder, filename)
+                df.to_csv(output_file_path, index=False)
+
+        except pd.errors.EmptyDataError:
+            pass
+
+
+def file_size_validation(output_folder, **kwargs):
     try:
-        csv_file_path = Data_Path
-        if csv_file_path.endswith('.csv'):
-            logger.info("File Type Validation is successful")
-            return True
-        else:
-            logger.error("File Type Validation failed: Not a CSV file")
-            return False
-    except Exception as e:
-        logger.error("File Type Validation failed: %s", str(e))
-        return False
+        for filename in os.listdir(output_folder):
+            file_path = os.path.join(output_folder, filename)
 
-def file_size_validation(**kwargs):
-    try:
-        csv_file_path = Data_Path
+            file_size_byte = os.path.getsize(file_path)
+            file_size_kb = file_size_byte / 1024
 
-        file_size_byte = os.path.getsize(csv_file_path)
-
-        file_size_kb = file_size_byte / 1024
-
-        if 0 < file_size_kb <= 2000:
-            logger.info("File Size Validation is successful")
+            if 0 < file_size_kb <= 2000:
+                logger.info("File Size Validation is successful for file: %s", filename)
+            else:
+                logger.warning("File Size Validation failed for file: %s", filename)
  
     except Exception as e:
         logger.error("File size Validation failed: %s", str(e))
 
-def column_header_validation(**kwargs):
-    try:
-        required_columns = [
-            "App", "Category", "Rating", "Reviews", "Size",
-            "Installs", "Type", "Price", "Content Rating",
-            "Genres", "Last Updated", "Current Ver", "Android Ver"
-        ]
-        
-        csv_file_path = Data_Path
-        df = pd.read_csv(csv_file_path)
-        columns = df.columns.tolist()
-        
-        if all(col in columns for col in required_columns):
-            logger.info("Column Validation is successful")
-            return True
-        else:
-            missing_columns = [col for col in required_columns if col not in columns]
-            logger.error("Column Validation failed: Missing columns - %s", missing_columns)
-            return False
-    except Exception as e:
-        logger.error("Column Validation failed: %s", str(e))
-        return False
-    
+
 with DAG(
     dag_id = "airflow_project_testing",
     schedule_interval='@daily',
-    start_date=datetime(2023,11,1),
+    start_date=datetime(2023,11,8),
     catchup=False
 ) as dag:
 
     File_type_validation = PythonOperator(
-        task_id='file_extention_validation',
-        python_callable=file_type_validation,
+        task_id='file_validation',
+        python_callable=process_csv_files,
         provide_context=True,
     )
 
     File_Size_Validation = PythonOperator(
         task_id='file_size_validation',
         python_callable=file_size_validation,
+        op_args=['/home/bipin/FInal-Project-/Data/Raw_Data'],
         provide_context=True,
     )
 
-    column_header_validation = PythonOperator(
-        task_id='column_header_validation',
-        python_callable=column_header_validation,
-        provide_context=True,
-    )
-     
     Raw_Data_To_Db = BashOperator(
         task_id='Raw_Data_Dump',
         bash_command="spark-submit {{ var.value.extraction_script }}",
@@ -106,10 +90,11 @@ with DAG(
         task_id = 'Transform_And_Load',
         bash_command = "spark-submit {{ var.value.Transformation_Script }}"
     )
-    
+
     Business_metrics = BashOperator(
         task_id = 'Business_Metrics_Generation',
         bash_command = "spark-submit {{var.value.Business_metrics_script}}"
     )
+
     
-    File_type_validation >>  File_Size_Validation >> column_header_validation >> Raw_Data_To_Db >> Transform_And_Load >> Business_metrics
+    File_type_validation >>  File_Size_Validation >> Raw_Data_To_Db >> Transform_And_Load >> Business_metrics
